6cb1694a40a8b051384cbf26c54cb7589691e019
Kehinde Yishawu <fkennyf@gmail.com>
2022-10-15 00:49:37 +0100

    imported pages from theme website and stabilize some styles

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 
 // config
+require("dotenv").config()
 app.use(express.static(__dirname + "/public"))
-// app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs")
 
 // route listeners
-
+app.get("/", (req, res)=>{
+    res.render("index")
+})
+app.get("/post", (req, res)=>{
+    res.render("blog-posts")
+})
+app.get("/contact", (req, res)=>{
+    res.render("contact")
+})
 
 // Server initialization
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server has started running")
+    console.log(`Server has started running on ${process.env.PORT}`)
 })
